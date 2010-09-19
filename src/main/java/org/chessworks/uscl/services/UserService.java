fcef411d763ee552e82004642e90fc268a62aaa8
doug.bateman <doug.bateman@gmail.com>
2010-09-19 08:20:52 +0000

    Changing how users and roles are mapped.

--- src/main/java/org/chessworks/uscl/services/UserService.java
+++ src/main/java/org/chessworks/uscl/services/UserService.java
@@ -9,22 +9,22 @@ import org.chessworks.uscl.services.InvalidNameException;
 
 public interface UserService {
 
-	void addUserToRole(User user, Role role);
+	User findUser(String handle);
 
 	Collection<User> findAllKnownUsers();
 
-	Role findOrCreateRole(String role);
-
 	Collection<User> findRegisteredUsers();
 
-	Role findRole(String role);
+	void register(User user) throws InvalidNameException;
 
-	User findUser(String handle);
+	Role findOrCreateRole(String role);
+
+	Role findRole(String role);
 
 	Set<User> findUsersInRole(Role role);
 
 	boolean isUserInRole(User user, Role role);
 
-	void register(User user) throws InvalidNameException;
+	void addUserToRole(User user, Role role);
 
 }
