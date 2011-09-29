1d9612b0cfeaf6c6c1fa450be29e6ac8ab067bca
rodrigo.chess <rodrigo.chess@gmail.com>
2011-09-29 00:56:16 +0000

    command who

--- src/main/java/org/chessworks/uscl/USCLBot.java
+++ src/main/java/org/chessworks/uscl/USCLBot.java
@@ -1074,6 +1074,7 @@ public class USCLBot {
             command.sendAdminCommand("spoof {0} tell JudgeBot nowin", name);
             command.sendAdminCommand("reserve-game {0} {1}", name, board);
             command.sendCommand("observe {0}", name);
+            command.sendAdminCommand("set-other {0} busy 0", name);
             players.add(name);
         }
     }
