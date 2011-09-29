a949aad6b823a75815753b88865f23b24df3de4b
doug <doug@dougbateman.net>
2011-09-29 04:45:45 +0000

    fixing format for show-schedule

--- src/main/java/org/chessworks/uscl/USCLBot.java
+++ src/main/java/org/chessworks/uscl/USCLBot.java
@@ -677,10 +677,10 @@ public class USCLBot {
         msg.format(" Current Schedule:\\n");
         for (Game game : games) {
     		int boardNum = game.boardNumber;
-    		String whiteStatus = (game.whitePlayer.getStatus().isOnline()) ? "+" : "?";
-    		String whitePlayer = whiteStatus + game.whitePlayer.getHandle();
-    		String blackStatus = (game.blackPlayer.getStatus().isOnline()) ? "+" : "?";
-    		String blackPlayer = blackStatus + game.blackPlayer.getHandle();
+    		String whiteStatus = (game.whitePlayer.isOnline()) ? "+" : "?";
+    		String whitePlayer = game.whitePlayer.getHandle() + whiteStatus;
+    		String blackStatus = (game.blackPlayer.isOnline()) ? "+" : "?";
+    		String blackPlayer = game.blackPlayer.getHandle() + blackStatus;
     		String gameStatus = game.getStatusString();
     		msg.format("Board %2d: %16s %16s - %s\\n", boardNum, whitePlayer, blackPlayer, gameStatus);
         }
