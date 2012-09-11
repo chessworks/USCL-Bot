84c3110fcbcd688a0c336f282b5e98b3bc5681c0
doug <doug@dougbateman.net>
2012-09-11 04:07:19 +0000

    Adding test cases using security annotations.

--- src/test/java/org/chessworks/uscl/TestCommandDispatcher.java
+++ src/test/java/org/chessworks/uscl/TestCommandDispatcher.java
@@ -1,7 +1,13 @@
 package org.chessworks.uscl;
 
+import javax.annotation.security.DenyAll;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import org.chessworks.chess.model.Role;
 import org.chessworks.chess.model.User;
 import org.chessworks.chess.services.simple.SimpleUserService;
 import org.chessworks.uscl.model.Player;
@@ -11,10 +17,12 @@ import org.easymock.EasyMock;
 
 public class TestCommandDispatcher extends TestCase {
 
+	@RolesAllowed("user")
 	public static interface Sample {
 
 		public void cmdBasicTeller(String teller);
 
+		@PermitAll
 		public void cmdPlayerTeller(Player teller);
 
 		public void cmdUserTeller(User teller);
@@ -24,11 +32,22 @@ public class TestCommandDispatcher extends TestCase {
 		public void cmdSchedule(String teller, int boardNum, Player white, Player black);
 
 		public void cmdUnreserveGame(String teller, Player player);
+		
+		@RolesAllowed("manager")
+		public void cmdManagersOnly(User teller);
+		
+		@DenyAll
+		public void cmdDenyAll(User teller);
+		
+		@PermitAll
+		public void cmdPermitAll(User teller);
 
 	}
 
 	private SimpleUserService userService;
 	private SimpleTournamentService tournamentService;
+	private Role testRole;
+	private Role managerRole;
 	private User testUser;
 	private Team testTeam;
 	private Player testPlayer;
@@ -39,15 +58,25 @@ public class TestCommandDispatcher extends TestCase {
 	public void setUp() throws Exception {
 		userService = new SimpleUserService();
 		tournamentService = new SimpleTournamentService();
+		testRole = userService.findOrCreateRole("user");
+		managerRole = userService.findOrCreateRole("manager");
 		testUser = userService.findUser("TestUser");
 		testTeam = tournamentService.createTeam("XYZ");
 		testPlayer = tournamentService.createPlayer("TestPlayer-XYZ");
 		testPlayer2 = tournamentService.createPlayer("TestPlayer2-XYZ", testTeam);
+		userService.addUserToRole(testUser,  testRole);
 		mock = EasyMock.createStrictMock(Sample.class);
 		dispatch = new CommandDispatcher();
-		dispatch.setTarget(mock);
 		dispatch.setTournamentService(tournamentService);
 		dispatch.setUserService(userService);
+		dispatch.setTarget(mock);
+		dispatch.init();
+	}
+	
+	public void testRoles() {
+		Assert.assertTrue(userService.isUserInRole(testUser,  testRole));
+		Assert.assertFalse(userService.isUserInRole(testUser,  managerRole));
+		Assert.assertTrue(userService.isUserInAnyRole(testUser,  new Role[] {testRole, managerRole}));
 	}
 
 	public void testBasicTeller() throws Exception {
@@ -90,6 +119,39 @@ public class TestCommandDispatcher extends TestCase {
 		mock.cmdUnreserveGame("TestUser", testPlayer);
 		dispatch.dispatch("TestUser", "reserve-game TestPlayer-XYZ 5");
 		dispatch.dispatch("TestUser", "unreserve-game TestPlayer-XYZ");
+		EasyMock.verify();
+	}
+
+	public void testManagersOnly() throws Exception {
+		EasyMock.replay();
+		try {
+			dispatch.dispatch("TestUser", "managers-only");
+			Assert.fail();
+		} catch (SecurityException e) {
+			//success
+		}
+		EasyMock.verify();
+	}
+
+	public void testDenyAll() throws Exception {
+		EasyMock.replay();
+		try {
+			dispatch.dispatch("TestUser", "deny-all");
+			Assert.fail();
+		} catch (SecurityException e) {
+			//success
+		}
+		EasyMock.verify();
+	}
+
+	public void testUsersOnly() throws Exception {
+		EasyMock.replay();
+		try {
+			dispatch.dispatch("Guest", "basic-teller");
+			Assert.fail();
+		} catch (SecurityException e) {
+			//success
+		}
 		EasyMock.verify();
 	}
 
