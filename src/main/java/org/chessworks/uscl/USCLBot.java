72be3066e899d81e0e0a7a7b690c0d7cedd4e0c5
doug <doug@dougbateman.net>
2014-10-09 04:11:27 +0000

    Version 17 is stable.  Incrementing to 18.

--- src/main/java/org/chessworks/uscl/USCLBot.java
+++ src/main/java/org/chessworks/uscl/USCLBot.java
@@ -67,7 +67,7 @@ public class USCLBot {
     public static final String BOARDS_FILE = "Games.txt";
     public static final String BOT_RELEASE_DATE = "October 8, 2014";
     public static final String BOT_RELEASE_NAME = "USCL-Bot";
-    public static final String BOT_RELEASE_NUMBER = "1.17";
+    public static final String BOT_RELEASE_NUMBER = "1.18";
     public static final PrintStream ECHO_STREAM = System.out;
     public static final int CHANNEL_USCL = 129;
     public static final int CHANNEL_CHESS_FM = 165;
